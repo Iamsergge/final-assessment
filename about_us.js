371a95ff2ba480a0fd70b0780097d8f198c9665e
Iamsergge <104618179+Iamsergge@users.noreply.github.com>
2023-03-01 07:22:56 +0200

    Add files via upload

---
+++ about_us.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+    ["South African","Black","Male","9509045471084"];
+
+    document.getElementById("Bio").innerHTML=["South African","Black","Male","9509045471084"];
+   
+};
---
+++ index.js
@@ -0,0 +1,14 @@
+
+window.onload = function () {
+    //  SECTION - HOME PAGE
+    let firstName = 'Serge';
+    let position = ' And Im a trainee front-end developer.';
+    
+    
+    document.getElementById('firstName').innerHTML = 'Hello I am ' + firstName + position;
+    document.getElementById('position').innerHTML = position;
+    document.getElementById('gender').innerHTML = gender;
+};
+
+   
+    
